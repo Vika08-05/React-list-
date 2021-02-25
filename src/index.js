c0da19ff7a32390273964d5213723b6c31e06468
Vika <vikusagerman@gmail.com>
2021-02-25 21:00:00 +0200

    React list

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React, { Fragment, Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+//Components
+import Header from "./Components/Header/header"
+import Search from "./Components/Search/search"
+import ContactList from "./Components/ContactList/contactList"
+import Footer from "./Components/Footer/footer"
+
+
+class App extends Component {
+
+  state = {
+    List: [
+      {
+        "Avatar": "https://www.bootdey.com/img/Content/avatar/avatar3.png",
+        "Name": "Mila Kunis",
+        "Created": "2013/08/08",
+        "Role": "Admin",
+        "Status": "Active",
+        "Email": "mila@kunis.com",
+      },
+      {
+        "Avatar": "https://www.bootdey.com/img/Content/avatar/avatar2.png",
+        "Name": "Camil Jonson",
+        "Created": "2013/02/08",
+        "Role": "User",
+        "Status": "Inctive",
+        "Email": "camil@gmail.com",
+      }
+    ]
+
+  }
+
+
+  render() {
+    const { List } = this.state;
+    return (
+      <Fragment>
+        <Header />
+        <Search />
+        <ContactList List={List} />
+        <Footer />
+      </Fragment>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
